4e75790ae047368d1e651ce18ad7adb991898643
Adama Fofana <mafahfof55@gmail.com>
2022-11-08 23:07:57 +0100

    page footer mise en forme

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,5 +1,17 @@
+import React from "react"
+import LogoFooter from '../../assets/LogoFooter.png'
+import '../../styles/Footer.css'
+
+
 function Footer() {
-    return <p> C'est ici le footer de la page</p>
+    return (
+        <footer>
+            <div id="box-footer">
+                <img src={LogoFooter} alt="Logo" />
+                <p>&copy; 2022 Kasa. Tous droits réservés</p>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
