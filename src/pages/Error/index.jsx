ab1d38606eb1d4523a97fcba4f412fffa8fb1341
Adama Fofana <mafahfof55@gmail.com>
2022-11-09 19:55:16 +0100

    mise en forme page 404

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,16 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import '../../styles/Error.css'
 
 
 function Error() {
-    return <div> Erreur</div>
+    return (
+        <div className="page-404">
+            <h1 className="titre-404">404</h1>
+            <span className="description-404">Oups! La page que vous demandez n'existe pas.</span>
+            <NavLink to="/" className="lien-site lien-404">Retourner sur la page d’accueil</NavLink>
+        </div>
+    )
 }
 
 export default Error
